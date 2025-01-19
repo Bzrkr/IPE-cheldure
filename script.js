78de0e824d871b6d5591cfac41e4f50c26f241ac
Alex <42751891+Bzrkr@users.noreply.github.com>
2025-01-19 18:38:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,128 +1,134 @@
 const dayNames = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-const IPEauditories = ["502-2 к.", "601-2 к.", "603-2 к.", "604-2 к.", "605-2 к.", "607-2 к.", "611-2 к.", "613-2 к.", "615-2 к."];
+        const IPEauditories = ["502-2 к.", "601-2 к.", "603-2 к.", "604-2 к.", "605-2 к.", "607-2 к.", "611-2 к.", "613-2 к.", "615-2 к."];
 
-
-
-async function fetchJson(url) {
-    const response = await fetch(url);
-    return response.json();
-}
-
-async function getTeacherInfo() {
-    const teachers = await fetchJson('https://iis.bsuir.by/api/v1/employees/all');
-    const teacherSchedules = {};
-
-    const promises = teachers.map(async (teacher) => {
-        try {
-            const schedule = await fetchJson(`https://iis.bsuir.by/api/v1/employees/schedule/${teacher.urlId}`);
-            teacherSchedules[teacher.urlId] = schedule;
-        } catch (error) {
-            console.error(`${teacher.urlId} generated an exception:`, error);
+        async function fetchJson(url) {
+            const response = await fetch(url);
+            return response.json();
         }
-    });
 
-    await Promise.all(promises);
-    return { teachers, teacherSchedules };
-}
+        async function getTeacherInfo() {
+            const teachers = await fetchJson('https://iis.bsuir.by/api/v1/employees/all');
+            const teacherSchedules = {};
 
-function parseDate(dateStr) {
-    return dateStr ? new Date(dateStr.split('.').reverse().join('-')) : null;
-}
+            const promises = teachers.map(async (teacher) => {
+                try {
+                    const schedule = await fetchJson(`https://iis.bsuir.by/api/v1/employees/schedule/${teacher.urlId}`);
+                    teacherSchedules[teacher.urlId] = schedule;
+                } catch (error) {
+                    console.error(`${teacher.urlId} generated an exception:`, error);
+                }
+            });
 
-function addLessonToSchedule(schedule, lesson, teacher) {
-    schedule[`${lesson.startLessonTime}—${lesson.endLessonTime}`] = `${lesson.subject} (${lesson.lessonTypeAbbrev}) ${teacher.fio}`;
-}
+            await Promise.all(promises);
+            return { teachers, teacherSchedules };
+        }
 
-function timeInRange(start, end, x) {
-    return start <= x && x <= end;
-}
+        function parseDate(dateStr) {
+            return dateStr ? new Date(dateStr.split('.').reverse().join('-')) : null;
+        }
 
-async function requestDaily(aud, teachers, teacherSchedules, currentWeek, selectedDate) {
-    const schedule = {};
-    const dayName = dayNames[selectedDate.getDay()];
+        function addLessonToSchedule(schedule, lesson, teacher) {
+            schedule[`${lesson.startLessonTime}—${lesson.endLessonTime}`] = `${lesson.subject} (${lesson.lessonTypeAbbrev}) ${teacher.fio}`;
+        }
 
-    for (const teacher of teachers) {
-        const teacherSchedule = teacherSchedules[teacher.urlId] || {};
-        const weekDaySchedule = teacherSchedule.schedules?.[dayName] || [];
+        function timeInRange(start, end, x) {
+            return start <= x && x <= end;
+        }
 
-        for (const lesson of weekDaySchedule) {
-            let weekNumbers = lesson?.weekNumber || [];
-            if (lesson && lesson.auditories && lesson.auditories.includes(aud) && currentWeek !== null && Array.isArray(weekNumbers) && weekNumbers.includes(currentWeek)) {
-                const start = parseDate(lesson.startLessonDate);
-                const end = parseDate(lesson.endLessonDate);
-                const lessonDate = parseDate(lesson.dateLesson);
+        async function requestDaily(aud, teachers, teacherSchedules, selectedDate, isPrevious = false) {
+            const schedule = {};
+            const dayName = dayNames[selectedDate.getDay()];
 
-                if (start && end && timeInRange(start, end, selectedDate)) {
-                    addLessonToSchedule(schedule, lesson, teacher);
-                } else if (lessonDate && selectedDate.toDateString() === lessonDate.toDateString()) {
-                    addLessonToSchedule(schedule, lesson, teacher);
+            for (const teacher of teachers) {
+                const teacherSchedule = teacherSchedules[teacher.urlId] || {};
+                const weekDaySchedule = isPrevious ? teacherSchedule.previousSchedules?.[dayName] || [] : teacherSchedule.schedules?.[dayName] || [];
+
+                for (const lesson of weekDaySchedule) {
+                    
+                    if (lesson && lesson.auditories && lesson.auditories.includes(aud)) {
+                        const start = parseDate(lesson.startLessonDate);
+                        const end = parseDate(lesson.endLessonDate);
+                        const lessonDate = parseDate(lesson.dateLesson);
+
+                        if (start && end && timeInRange(start, end, selectedDate)) {
+                            addLessonToSchedule(schedule, lesson, teacher);
+                        } else if (lessonDate && selectedDate.toDateString() === lessonDate.toDateString()) {
+                            addLessonToSchedule(schedule, lesson, teacher);
+                        }
+                    }
                 }
             }
+            return schedule;
         }
-    }
-    return schedule;
-}
 
-function printDict(container, dict) {
-    for (const [timeRange, details] of Object.entries(dict)) {
-        const lessonDiv = document.createElement('div');
-        lessonDiv.className = 'lesson';
-        lessonDiv.innerText = `${timeRange} ————— ${details}`;
-        container.appendChild(lessonDiv);
-    }
-}
+        function printDict(container, dict) {
+            for (const [timeRange, details] of Object.entries(dict)) {
+                const lessonDiv = document.createElement('div');
+                lessonDiv.className = 'lesson';
+                lessonDiv.innerText = `${timeRange} ————— ${details}`;
+                container.appendChild(lessonDiv);
+            }
+        }
 
-async function printSchedulesIPE(selectedDate) {
-    const { teachers, teacherSchedules } = await getTeacherInfo();
-    const currentWeek = await fetchJson('https://iis.bsuir.by/api/v1/schedule/current-week');
-    const schedulesContainer = document.getElementById('schedules');
-    schedulesContainer.innerHTML = ''; // Clear previous schedules
+        async function printSchedulesIPE(selectedDate) {
+            const { teachers, teacherSchedules } = await getTeacherInfo();
+            
+            const schedulesContainer = document.getElementById('schedules');
+            schedulesContainer.innerHTML = ''; // Clear previous schedules
 
-       for (const aud of IPEauditories) {
-        const audContainer = document.createElement('div');
-        audContainer.className = 'auditory';
-        audContainer.innerText = `-------------------------${aud}-------------------------`;
-        schedulesContainer.appendChild(audContainer);
+            for (const aud of IPEauditories) {
+                const audContainer = document.createElement('div');
+                audContainer.className = 'auditory';
+                audContainer.innerText = `-------------------------${aud}-------------------------`;
+                schedulesContainer.appendChild(audContainer);
 
-        const dailySchedule = await requestDaily(aud, teachers, teacherSchedules, currentWeek, selectedDate);
-        const sortedSchedule = Object.keys(dailySchedule).sort().reduce((obj, key) => {
-            obj[key] = dailySchedule[key];
-            return obj;
-        }, {});
+                // Получаем текущее расписание
+                const currentSchedule = await requestDaily(aud, teachers, teacherSchedules,selectedDate);
+                // Получаем предыдущее расписание
+                const previousSchedule = await requestDaily(aud, teachers, teacherSchedules, selectedDate, true);
 
-        printDict(audContainer, sortedSchedule);
-    }
-}
+                // Объединяем расписания
+                const combinedSchedule = { ...currentSchedule, ...previousSchedule };
 
-document.getElementById('datePicker').addEventListener('change', async (event) => {
-    const selectedDate = new Date(event.target.value);
-    document.getElementById('loading').style.display = 'block'; // Показываем окно "Загрузка..."
-    try {
-      await printSchedulesIPE(selectedDate);
-    } finally {
-      document.getElementById('loading').style.display = 'none'; // Скрываем окно "Загрузка..."
-    }
-  });
+                // Сортируем расписание по времени
+                const sortedSchedule = Object.keys(combinedSchedule).sort().reduce((obj, key) => {
+                    obj[key] = combinedSchedule[key];
+                    return obj;
+                }, {});
 
-// Initialize with the current date
-const initialDate = new Date();
-document.getElementById('datePicker').valueAsDate = initialDate;
-document.getElementById('loading').style.display = 'block'; // Показываем окно "Загрузка..."
-(async () => {
-  try {
-    await printSchedulesIPE(initialDate);
-  } finally {
-    document.getElementById('loading').style.display = 'none'; // Скрываем окно "Загрузка..."
-  }
-})();
+                printDict(audContainer, sortedSchedule);
+            }
+        }
 
-function copyAndSend() {
-    const textToCopy = document.getElementById('schedules').innerText;
-    navigator.clipboard.writeText(textToCopy).then(() => {
-        alert('Текст скопирован!');
-        const telegramLink = `tg://msg?text=${encodeURIComponent(textToCopy)}`;
-        window.open(telegramLink, '_blank');
-    }).catch(err => {
-        console.error('Ошибка при копировании текста: ', err);
-    });
-}
+        document.getElementById('datePicker').addEventListener('change', async (event) => {
+            const selectedDate = new Date(event.target.value);
+            document.getElementById('loading').style.display = 'block'; // Показываем окно "Загрузка..."
+            try {
+                await printSchedulesIPE(selectedDate);
+            } finally {
+                document.getElementById('loading').style.display = 'none'; // Скрываем окно "Загрузка..."
+            }
+        });
+
+        // Initialize with the current date
+        const initialDate = new Date();
+        document.getElementById('datePicker').valueAsDate = initialDate;
+        document.getElementById('loading').style.display = 'block'; // Показываем окно "Загрузка..."
+        (async () => {
+            try {
+                await printSchedulesIPE(initialDate);
+            } finally {
+                document.getElementById('loading').style.display = 'none'; // Скрываем окно "Загрузка..."
+            }
+        })();
+
+        function copyAndSend() {
+            const textToCopy = document.getElementById('schedules').innerText;
+            navigator.clipboard.writeText(textToCopy).then(() => {
+                alert('Текст скопирован!');
+                const telegramLink = `tg://msg?text=${encodeURIComponent(textToCopy)}`;
+                window.open(telegramLink, '_blank');
+            }).catch(err => {
+                console.error('Ошибка при копировании текста: ', err);
+            });
+        }
